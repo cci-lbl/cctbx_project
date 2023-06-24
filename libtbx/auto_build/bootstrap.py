39ef33809fafda3a61fd7da7e8a75fd684b81467
Billy K. Poon <bkpoon@lbl.gov>
2023-06-24 10:55:25 -0700

    bootstrap: use older commit for iota

--- libtbx/auto_build/bootstrap.py
+++ libtbx/auto_build/bootstrap.py
@@ -2450,6 +2450,10 @@ class PhenixBuilder(CCIBuilder):
       self.add_step(self.shell(
         command=['git', 'branch', '--set-upstream-to=origin/dials-3.8', 'dials-3.8'],
         workdir=workdir))
+    if module == 'iota' and not self.python3:
+      self.add_step(self.shell(
+        command=['git', 'checkout', '5c2792911730855ccf91308a24db8af6a8f302be'],
+        workdir=workdir))
 
   def add_module(self, module, workdir=None, module_directory=None):
     """
